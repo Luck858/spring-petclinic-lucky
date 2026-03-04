5754efa20d0fc1b76f2615a723431fd1176abd14
lucky varma <neelamlakshminarayana88@gmail.com>
2026-03-04 16:29:08 +0530

    added whole cicd pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-rg"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "devops-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "devopsaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
