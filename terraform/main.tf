1b3189a1e002afddc6f424b10a97bfeba307b601
Neelam Lakshmi Narayana <123484424+Luck858@users.noreply.github.com>
2026-03-05 12:49:15 +0530

    fix terrafom
    Signed-off-by: Neelam Lakshmi Narayana <123484424+Luck858@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,37 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "devops-rg"
-  location = "East US"
+provider "azurerm" {
+  features {}
 }
 
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "myRG"
+  location = "centralindia"
+}
+
+# AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "devops-aks"
+  name                = "myAKSCluster"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "devopsaks"
+  dns_prefix          = "myakscluster"
 
   default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_B2s"
+    name       = "nodepool1"
+    node_count = 1
+    vm_size    = "Standard_D2s_v3"
   }
 
   identity {
     type = "SystemAssigned"
   }
+
+  tags = {
+    Environment = "DevOps-Learning"
+  }
+}
+
+# Output kubeconfig
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
 }
